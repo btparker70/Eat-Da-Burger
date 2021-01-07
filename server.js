efd6c3dcb7acc584f10e4f980779ca0de0bb7352
Brian Parker <btparker70@gmail.com>
2021-01-07 14:05:13 -0800

    set up server.js and added .env for password

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+require('dotenv').config();
+
+// Port
+var PORT = process.env.PORT || 3000;
+
+// Node server
+var app = express();
+
+// Server static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give server access to them
+var routes = require("./controllers/burgers_controller.js")
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+})
